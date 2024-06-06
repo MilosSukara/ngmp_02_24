41a91581c0b725c944159638cfd6296e47353ea1
Milos Sukara <milos.sukara@digitalinsurance.io>
2024-06-06 18:01:24 +0200

    Module 01 task implementation

--- module_01/index.js
+++ module_01/index.js
@@ -1,7 +1,15 @@
-function randomNumberMinMax(min, max) {
-  
+const MIN = 1;
+const MAX = 1000;
+
+
+function randomIntegerMinMax(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max + 1); // Making the max value inclusive
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
 }
 
 export default function getRandomNumber() {
-
+  return randomIntegerMinMax(MIN, MAX);
 }
+
+console.log(getRandomNumber());
