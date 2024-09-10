4b4d334bfdcbc63dfc9d57300e4dd64956a1e632
Milos Sukara <Milos.Sukara@informa.com>
2024-09-10 15:43:07 +0200

    Cleaned up README and removed watcher

--- module_09/ecosystem.config.js
+++ module_09/ecosystem.config.js
@@ -3,8 +3,8 @@ module.exports = {
     name: "ngmp-express-app",
     script: "src/index.ts",
     interpreter: "node_modules/.bin/ts-node",
-    watch: true,
-    ignore_watch: ["node_modules"],
+    // watch: true,
+    // ignore_watch: ["node_modules"],
     exec_mode: 'fork',
     instances: '1',
     env: {
